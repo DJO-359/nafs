844281b2e65fedb2a7c77d84c504dfad8260f760
Dzhabrail <fredom358@gmail.com>
2026-08-07 17:34:51 +0300

    2323

--- frontend/src/pages/DayHistoryPage.tsx
+++ frontend/src/pages/DayHistoryPage.tsx
@@ -33,7 +33,7 @@ export default function DayHistoryPage() {
   useBackButton();
 
   return (
-    <div className="bott">
+    <div className="pb-16">
       <QueryState query={query}>
         {(day) => {
           const diaryEntry = day.diary as unknown as DiaryEntry | null;
