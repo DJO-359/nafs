98266c2d30b88fcf40eed4b934e1959f987d24ad
Dzhabrail <fredom358@gmail.com>
2026-08-18 14:16:55 +0300

    dfdf

--- backend/src/tasbih/tasbih.module.ts
+++ backend/src/tasbih/tasbih.module.ts
@@ -4,9 +4,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { TasbihCounter } from './models/tasbih-counter.model';
 import { TasbihService } from './tasbih.service';
 import { TasbihController } from './tasbih.controller';
+import { User } from '../users/models/user.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([TasbihCounter])],
+  imports: [SequelizeModule.forFeature([TasbihCounter, User])],
   controllers: [TasbihController],
   providers: [TasbihService],
   exports: [TasbihService],
